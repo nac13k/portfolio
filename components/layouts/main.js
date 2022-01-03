f8c5f19e372afe8994d5075284c0a5916369647e
Lumbreras <nac13k@gmail.com>
2022-01-02 22:28:15 -0600

    add menu to layout

--- components/layouts/main.js
+++ components/layouts/main.js
@@ -1,26 +1,30 @@
 import Head from 'next/head'
 import { Box, ColorModeScript, Container } from '@chakra-ui/react'
-import ColorMode from './color-mode'
 import theme from '../../lib/theme'
+import MenuConfig from '../../lib/menu-config'
+import Navbar from './navbar'
+
 
 const Main = ({ children, router }) => {
   return (
-    <Box as="main" pb={8}>
-      <Head>
-        <meta name="viewport" content="width=device-width, initial-scale=1"/>
-        <title>Lumbreras &lt;@nac13k&gt;</title>
-      </Head>
+    <>
+      <Navbar links={MenuConfig.routes} brandName={MenuConfig.brandName}></Navbar>
+      <Box as="main" pb={8}>
+        <Head>
+          <meta name="viewport" content="width=device-width, initial-scale=1"/>
+          <title>Lumbreras &lt;@nac13k&gt;</title>
+        </Head>
 
-      <body>
-        <ColorModeScript initialColorMode={theme.config.initialColorMode} />
+        <body>
+          <ColorModeScript initialColorMode={theme.config.initialColorMode} />
           <Box align="right" p={3}>
-            <ColorMode/>
           </Box>
-        <Container maxW="container.md" pt={14}>
-          {children}
-        </Container>
-      </body>
-    </Box>
+          <Container maxW="container.md" pt={14}>
+            {children}
+          </Container>
+        </body>
+      </Box>
+      </>
   )
 }
 
